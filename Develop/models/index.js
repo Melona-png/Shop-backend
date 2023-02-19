84bfcc0d7dce500eb2e02aba95f18eedbc2cb254
Melona-Png <madisont.woodward@gmail.com>
2023-02-18 22:21:20 -0500

    trying to figure out why I cant seed

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,12 +1,42 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('./sequelize');
 
-// Product model
+// Define models
 class Product extends Model {}
 Product.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true,
+    },
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'id',
+    },
+  },
 }, {
   sequelize,
   modelName: 'product'
@@ -23,6 +53,16 @@ Tag.belongsToMany(Product, { through: ProductTag });
 // Category model
 class Category extends Model {}
 Category.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 }, {
   sequelize,
   modelName: 'category'
@@ -31,6 +71,15 @@ Category.init({
 // Tag model
 class Tag extends Model {}
 Tag.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  tag_name: {
+    type: DataTypes.STRING,
+  },
 }, {
   sequelize,
   modelName: 'tag'
@@ -39,11 +88,30 @@ Tag.init({
 // ProductTag model
 class ProductTag extends Model {}
 ProductTag.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product',
+      key: 'id',
+    },
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'tag',
+      key: 'id',
+    },
+  },
 }, {
   sequelize,
   modelName: 'product_tag'
 });
-
 
 module.exports = {
   Product,
